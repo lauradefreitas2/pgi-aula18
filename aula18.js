239baff9abbf3ceafe0c6d15ce46ebb337b6084c
lauradefreitas2 <lauradovalegameplays@gmail.com>
2022-06-10 16:56:48 -0300

    questao 4 e 5

--- aula18.js
+++ aula18.js
@@ -53,3 +53,48 @@ let somar = function (numA , numB, numC){
  somar(4,5,5,)
  
 let resultado = 0;
+
+// você deve criar uma função chamada newArray que receba um número como
+// parâmetro e retorne um novo array com a quantidade de elementos equivalente ao
+// valor do número que você passou por parâmetro.
+// Exemplo:
+// newArray(5) deve retornar [1,2,3,4,5]
+// newArray(10) deve retornar [1,2,3,4,5,6,7,8,9,10]
+
+function newArray(a){
+   b = [] 
+    for(var i=1; i<=a; i++){
+      b.push(i)
+    }
+    return b
+}
+console.log(newArray(5));
+
+// 5. String.split()
+// Você deve criar uma função chamada split que receba uma string e simule o
+// comportamento da função split original. Se você não sabe como funciona, o Google
+// pode ajudá-lo.
+// Importante: Você não pode usar o String.split()
+// Exemplo:
+// split(“olá”) deve retornar [”o”,”l”,”á”]
+// split(tchau) deve retornar [“t”,“c”,”h”,”a”,”u”
+
+function split (a) {
+    let array = [];
+    for (let i = 0; i < a.length; i++) {
+        array.push(a[i]);
+    }
+    return array;
+ }
+ 
+ console.log(split("maça"));
+
+//  6. Mover os zeros para o final
+//  Você deve criar uma função chamada moverZeros que receba um array como
+//  parâmetro e retorne outro com os números 0 ordenados ao final.
+//  Exemplo:
+//  moverZeros([false,1,0,1,2,0,1,3,"a"]) deve retornar [false,1,1,2,1,3,"a",0,0]
+//  moverZeros([1,2,0,1,0,1,0,3,0,1]) deve retornar [1,2,1,1,3,1,0,0,0,0]
+
+
+ 
